421929efe53961d917738cdc0619afff8ed389ad
fedorayui <550149998@qq.com>
2026-08-04 16:27:44 -0400

    Finished challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -123,3 +123,32 @@ for (const odd of Object.values(game.odds)) {
   average /= Object.values(game.odds).length;
   console.log(average);
 }
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// Challenge 3
+// 1.
+const events = [...new Set(gameEvents.values())];
+
+// 2.
+gameEvents.delete(64);
+
+console.log(`An event happened, on average, every ${90/gameEvents.size} minutes`);
+
+
+for (const [key, value] of gameEvents) {
+  const half = key <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${key}: ${value}`);
+}
