0bbedbdfa9e1021273a810f96a8396d5d166a779
fedorayui <550149998@qq.com>
2026-09-04 16:07:59 -0400

    forEach & map function use

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,6 +76,14 @@ const displayMovements = function (movements) {
   });
 }
 displayMovements(account1.movements)
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('')
+  });
+}
+
+createUsernames(accounts)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -87,6 +95,13 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrawal'} ${Math.abs(
+      mov
+    )}`
+);
 
 /////////////////////////////////////////////////
 containerApp.style.opacity = 100;
